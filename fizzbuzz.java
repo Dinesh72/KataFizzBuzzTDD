b678dfd14d4e551a7bfebc9942e4c893555515f6
Dinesh72 <17bit168@nirmaunic.ac.in>
2020-09-23 02:41:05 +0530

    Update fizzbuzz.java
    using ternery condition

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -2,64 +2,34 @@ class  fizzbuzz{
 
     public static boolean contains_3(int number)
     {
-     if(number%10==3)
-         return true;
-         else
-         return false;
+        return ((number%10==3?true:false));
+//     if(number%10==3)
+//         return true;
+//         else
+//         return false;
     }
     public static boolean contains_5(int number)
     {
-     if(number%10==5)
-         return true;
-         else
-         return false;
+        return ((number%10==5?true:false));
+//     if(number%10==5)
+//         return true;
+//         else
+//         return false;
     }
     public static boolean isDivisibleby_3and5(int number)
 {
-    Boolean status=false;
-    Integer checknumber;
-   if(number%3==0 && number%5==0)
-   {
-       status=true;
-   }
-   else
-   {
-       status=false;
-   }
+    return ((number%3==0) && (number%5==0) ?true:false);
+//    Boolean status=false;
+//    Integer checknumber;
+//   if(number%3==0 && number%5==0)
+//   {
+//       status=true;
+//   }
+//   else
+//   {
+//       status=false;
+//   }
+//    
+//    return status;
     
-    return status;
-    
-}
-public static boolean isDivisibleby_3(int number)
-{
-    Boolean status=false;
-    Integer checknumber;
-   if(number%3==0)
-   {
-       status=true;
-   }
-   else
-   {
-       status=false;
-   }
-    
-    return status;
-    
-}
-public static boolean isDivisibleby_5(int number)
-{
-    Boolean status=false;
-    Integer checknumber;
-   if(number%5==0)
-   {
-       status=true;
-   }
-   else
-   {
-       status=false;
-   }
-    
-    return status;
-    
-}
 }
