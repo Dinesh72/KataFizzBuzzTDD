5e3ea33e8309ac3848469fa9408510f1d14926f6
Dinesh72 <17bit168@nirmaunic.ac.in>
2020-09-23 02:35:42 +0530

    Create fizzbuzz.java
    creaing method divisible by 3

---
+++ fizzbuzz.java
@@ -0,0 +1,19 @@
+class  fizzbuzz{
+
+public boolean isDivisibleby_3(int number)
+{
+    Boolean status=false;
+    Integer checknumber;
+   if(number%3==0)
+   {
+       status=true;
+   }
+   else
+   {
+       status=false;
+   }
+    System.out.println("Number "+number);
+    return status;
+    
+}
+}
