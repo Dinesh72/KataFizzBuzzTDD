78653e10f2db3835125d7639f68682bece811e0e
Dinesh72 <17bit168@nirmaunic.ac.in>
2020-09-23 02:39:50 +0530

    Update fizzbuzz.java
    adding methods to check it contain 3 and 5 in numbers

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -1,6 +1,19 @@
 class  fizzbuzz{
 
-  
+    public static boolean contains_3(int number)
+    {
+     if(number%10==3)
+         return true;
+         else
+         return false;
+    }
+    public static boolean contains_5(int number)
+    {
+     if(number%10==5)
+         return true;
+         else
+         return false;
+    }
     public boolean isDivisibleby_3and5(int number)
 {
     Boolean status=false;
