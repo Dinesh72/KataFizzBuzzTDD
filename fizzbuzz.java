49235dae07c5b54feb3a0c5920f36ed3f0e0b51c
Dinesh72 <17bit168@nirmaunic.ac.in>
2020-09-23 02:38:24 +0530

    Update fizzbuzz.java
    creating method to check both 3 and 5 divisible

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -1,5 +1,22 @@
 class  fizzbuzz{
 
+  
+    public boolean isDivisibleby_3and5(int number)
+{
+    Boolean status=false;
+    Integer checknumber;
+   if(number%3==0 && number%5==0)
+   {
+       status=true;
+   }
+   else
+   {
+       status=false;
+   }
+    
+    return status;
+    
+}
 public boolean isDivisibleby_3(int number)
 {
     Boolean status=false;
